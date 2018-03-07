60c29575e4011d44166f1021631396601638e651
22lrfuller <lfuller10@outlook.com>
2018-03-06 20:43:54 -0500

    Improved on the path creation, but it still isn't fully working.

--- _test_.py
+++ _test_.py
@@ -17,7 +17,6 @@ randDoor = randNum(1,8)
 randBlock = randNum(1,10)
 gameBoard = []
 entrancePos = []
-exitPos = []
 pathDone = False
 
 #Neutral Colors
@@ -34,30 +33,29 @@ green = (0, 210, 0)
 blue = (0, 125, 255)
 violet = (200, 0, 200)
 
-def isSteppable(funcX, funcY):
-	return gameBoard[funcX][funcY]
-
-def getBlockType(blockX, blockY): #Defining blocks in the grid
-	global entrancePos, exitPos
-	if(blockX == 0 or blockX == 9 or blockY == 0 or blockY == 9):
-		if(blockY == 0 and randDoor == blockX):
-			exitPos = [randDoor,blockY]
-			print(exitPos)
-			return "exit"
-		if(blockY == 9 and randDoor == blockX):
-			entrancePos = [randDoor,blockY-1]
-			return "entrance"
-		return "border"
-	randBlock = randNum(1,10)
-	if(randBlock <= 7): return "path"
-	if(randBlock > 7 and randBlock < 10): return "wall"
-	return "actionBlock"
+def getBlockType(blockX, blockY, blockKind): #Defining blocks in the grid
+	global entrancePos
+	if(blockKind == "origPath"):
+		randBlock = randNum(1,5)
+		if(randBlock <= 3): return "path"
+		return "actionBlock"
+	if(blockKind == "other"):
+		if(blockX == 0 or blockX == 9 or blockY == 0 or blockY == 9):
+			if(blockY == 9 and randDoor == blockX):
+				entrancePos = [randDoor,blockY]
+				print(entrancePos)
+				return "entrance"
+			return "border"
+		randBlock = randNum(1,10)
+		if(randBlock <= 1): return "path"
+		if(randBlock > 1 and randBlock < 9): return "wall"
+		return "actionBlock"
 
 def getDir(entrancePos):
 	global posPath
 	blockX = entrancePos[0]
-	blockY = entrancePos[1]
-	while(exitPos != [blockX,blockY-1]):
+	blockY = entrancePos[1]-1
+	while(blockY-1 != 0):
 		randomVar = randNum(1,3)
 		if(randomVar == 1 and gameBoard[blockX-1][blockY] != "border"): #Left
 			blockX -= 1
@@ -66,8 +64,12 @@ def getDir(entrancePos):
 		if(randomVar == 3 and gameBoard[blockX+1][blockY] != "border"): #Right
 			blockX += 1
 		posPath.append([blockX, blockY])
-	for x in posPath:
-		
+	exitPos = [blockX, blockY-1]
+	print(gameBoard)
+	for x in range(len(posPath)):
+		print(gameBoard[posPath[x][0]][posPath[x][1]])
+		gameBoard[posPath[x][0]][posPath[x][1]] = getBlockType(posPath[x][0],posPath[x][1],"origPath")
+	gameBoard[exitPos[0]][exitPos[1]] = "exit"
 
 def getColor(tileType): #Coloring the blocks
 	if(tileType == "border" or tileType == "wall"):
@@ -81,30 +83,30 @@ def getColor(tileType): #Coloring the blocks
 
 def getEntrance():
 	for x in range(len(gameBoard)):
-		for y in range(10):
+		for y in range(0,10):
 			if(gameBoard[x][y] == "entrance"):
 				return x, y
 
-def keyDown(event, funcX, funcY): #Start Movement on Key Down
+def keyUp(event, funcX, funcY): #Start Movement on Key Down
 	if(event.key == K_UP or event.key == K_w):
 		if(funcY-1 >= 0 and funcY-1 <= 9):
-			if(isSteppable(funcX, funcY-1) != "wall" and isSteppable(funcX,funcY-1) != "border"):
+			if(gameBoard[funcX][funcY-1] != "wall" and gameBoard[funcX][funcY-1] != "border"):
 				funcY -= 1
 	elif(event.key == K_DOWN or event.key == K_s):
 		if(funcY+1 >= 0 and funcY+1 <= 9):
-			if(isSteppable(funcX, funcY+1) != "wall" and isSteppable(funcX,funcY+1) != "border"):
+			if(gameBoard[funcX][funcY+1] != "wall" and gameBoard[funcX][funcY+1] != "border"):
 				funcY += 1
 	elif(event.key == K_LEFT or event.key == K_a):
 		if(funcX-1 >= 0 and funcX-1 <= 9):
-			if(isSteppable(funcX-1, funcY) != "wall" and isSteppable(funcX-1,funcY) != "border"):
+			if(gameBoard[funcX-1][funcY] != "wall" and gameBoard[funcX-1][funcY] != "border"):
 				funcX -= 1
 	elif(event.key == K_RIGHT or event.key == K_d):
 		if(funcX+1 >= 0 and funcX+1 <= 9):
-			if(isSteppable(funcX+1, funcY) != "wall" and isSteppable(funcX+1,funcY) != "border"):
+			if(gameBoard[funcX+1][funcY] != "wall" and gameBoard[funcX+1][funcY] != "border"):
 				funcX += 1
 	return funcX, funcY
 
-def keyUp(event): #Stop Movement on Key Lift
+def keyDown(event): #Stop Movement on Key Lift
 	pass
 
 #Creates the game board
@@ -113,7 +115,7 @@ posPath = []
 
 for loopY in range(0,10):
 	for loopX in range(0,10):
-		blockType = getBlockType(loopX,loopY)
+		blockType = getBlockType(loopX,loopY,"other")
 		gameBoard[loopX][loopY] = blockType
 	randDoor = randNum(1,8)
 
@@ -130,9 +132,9 @@ clock = pygame.time.Clock()
 while(gameOver == False): #Main Game Loop
 	for event in pygame.event.get():
 		if(event.type == pygame.KEYUP):
-			xBox, yBox = keyDown(event, xBox, yBox)
+			xBox, yBox = keyUp(event, xBox, yBox)
 		if(event.type == pygame.KEYDOWN):
-			keyUp(event)
+			keyDown(event)
 		if(event.type == pygame.QUIT):
 			gameOver = True
 
