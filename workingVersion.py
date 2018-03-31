e3d028c38189fbd57a5fd62b4f156456419c7f0d
jpk <_@jpk.is>
2018-03-31 19:40:02 -0400

    Corrects gameBoard coordinate order
     - Changes all the instances of gameBoard[x][y] to [y][x], since
       that was how the gameBoard is initalized

--- workingVersion.py
+++ workingVersion.py
@@ -88,43 +88,47 @@ def getDir(entrancePos): #Creating the random path
 	blockY = entrancePos[1]
 	while(blockY-1 != 0):
 		randomVar = randNum(1,3)
-		if(randomVar == 1 and gameBoard[blockX-1][blockY][0] != 'border'): #Left
+		if(randomVar == 1 and gameBoard[blockY][blockX-1][0] != 'border'): #Left
 			blockX -= 1
-		if(randomVar == 2 and gameBoard[blockX][blockY-1][0] != 'border'): #Up
+		if(randomVar == 2 and gameBoard[blockY-1][blockX][0] != 'border'): #Up
 			blockY -= 1
-		if(randomVar == 3 and gameBoard[blockX+1][blockY][0] != 'border'): #Right
+		if(randomVar == 3 and gameBoard[blockY][blockX+1][0] != 'border'): #Right
 			blockX += 1
 		posPath.append([blockX, blockY])
 	exitPos = [blockX, blockY-1]
-	for x in range(len(posPath)):
-		gameBoard[posPath[x][0]][posPath[x][1]] = [getBlockType(posPath[x][0],posPath[x][1],'origPath')]
-	gameBoard[exitPos[0]][exitPos[1]] = ['exit']
+	for i in range(len(posPath)):
+		gameBoard[posPath[i][1]][posPath[i][0]] = [getBlockType(posPath[i][0],posPath[i][1],'origPath')]
+	gameBoard[exitPos[1]][exitPos[0]] = ['exit']
 
 def keyUp(event, funcX, funcY): #Start Movement on Key Down
 	if(event.key == K_UP or event.key == K_w):
 		if(funcY-1 >= 0 and funcY-1 <= 9):
-			if(gameBoard[funcX][funcY-1][0] != 'wall' and gameBoard[funcX][funcY-1][0] != 'border'):
+			if(gameBoard[funcY-1][funcX][0] != 'wall' and gameBoard[funcY-1][funcX][0] != 'border'):
 				funcY -= 1
 	elif(event.key == K_DOWN or event.key == K_s):
 		if(funcY+1 >= 0 and funcY+1 <= 9):
-			if(gameBoard[funcX][funcY+1][0] != 'wall' and gameBoard[funcX][funcY+1][0] != 'border'):
+			if(gameBoard[funcY+1][funcX][0] != 'wall' and gameBoard[funcY+1][funcX][0] != 'border'):
 				funcY += 1
 	elif(event.key == K_LEFT or event.key == K_a):
 		if(funcX-1 >= 0 and funcX-1 <= 9):
-			if(gameBoard[funcX-1][funcY][0] != 'wall' and gameBoard[funcX-1][funcY][0] != 'border'):
+			if(gameBoard[funcY][funcX-1][0] != 'wall' and gameBoard[funcY][funcX-1][0] != 'border'):
 				funcX -= 1
 	elif(event.key == K_RIGHT or event.key == K_d):
 		if(funcX+1 >= 0 and funcX+1 <= 9):
-			if(gameBoard[funcX+1][funcY][0] != 'wall' and gameBoard[funcX+1][funcY][0] != 'border'):
+			if(gameBoard[funcY][funcX+1][0] != 'wall' and gameBoard[funcY][funcX+1][0] != 'border'):
 				funcX += 1
 	return funcX, funcY
 
 #Createing the game board
+# Ah-ha! The way this is written, gameBoard is definitely row-major with
+# indexes in [y][x] order because the x positions are in the inner arrays, and
+# the y positions are in the outer array.
+# (See https://en.wikipedia.org/wiki/Row-_and_column-major_order )
 gameBoard = [[[''] for x in range(10)] for y in range(10)]
 for loopY in range(0,10):
 	for loopX in range(0,10):
 		blockType = getBlockType(loopX,loopY,'other')
-		gameBoard[loopX][loopY][0] = blockType
+		gameBoard[loopY][loopX][0] = blockType
 	randDoor = randNum(1,8)
 
 getDir(entrancePos) #Generates Random Path
@@ -135,8 +139,8 @@ for loopY in range(0,10):
 		if(loopX == entrancePos[0] and loopY == entrancePos[1]):
 			art = getArt('entrance')
 		else:
-			art = getArt(gameBoard[loopX][loopY][0])
-		gameBoard[loopX][loopY].append(art)
+			art = getArt(gameBoard[loopY][loopX][0])
+		gameBoard[loopY][loopX].append(art)
 
 xBox, yBox = entrancePos #Sets the player's position
 
@@ -166,9 +170,9 @@ while(gameOver == False):
 		screen.blit(pygame.font.SysFont('calibri', 30).render('Start', False, red),(400,700))
 	elif(scene == 'game'):
 		#Prints the two-dimensional array to the screen
-		for x in range(len(gameBoard)):
-			for y in range(10):
-				screen.blit(pygame.transform.scale(pygame.image.load('gameArt/' + str(gameBoard[x][y][1]) + '.png'), (100,100)),(x*100, y*100))
+		for y in range(len(gameBoard)):
+			for x in range(len(gameBoard[y])):
+				screen.blit(pygame.transform.scale(pygame.image.load('gameArt/' + str(gameBoard[y][x][1]) + '.png'), (100,100)),(x*100, y*100))
 
 		screen.blit(playerImg,(xBox*100+12, yBox*100+12))
 
